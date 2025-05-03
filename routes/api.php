b5fc9c757991e42a98f1fdde2ddd8655b521c4ed
MoNashar <mohamad.nashar.maids.cc@gmail.com>
2025-05-03 09:30:42 +0300

    reach categories by slug, not by id

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,7 @@ Route::get('/posts', [Posts::class, 'index'])->name('posts.index');
 Route::get('/posts/{post}', [Posts::class, 'show'])->name('posts.show');
 
 Route::get('/categories', [Categories::class, 'index'])->name('categories.index');
-Route::get('/categories/{category}', [Categories::class, 'show'])->name('categories.show');
+Route::get('/categories/{category:slug}', [Categories::class, 'show'])->name('categories.show');
 Route::post('/categories', [Categories::class, 'store'])->name('categories.store');
-Route::put('/categories/{category}', [Categories::class, 'update'])->name('categories.update');
-Route::delete('/categories/{category}', [Categories::class, 'destroy'])->name('categories.destroy');
+Route::put('/categories/{category:slug}', [Categories::class, 'update'])->name('categories.update');
+Route::delete('/categories/{category:slug}', [Categories::class, 'destroy'])->name('categories.destroy');
