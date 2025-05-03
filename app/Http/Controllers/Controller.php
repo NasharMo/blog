d2da62b546223b1fed3f977778b43e397ab36b1e
MoNashar <mohamad.nashar.maids.cc@gmail.com>
2025-05-03 22:43:48 +0300

    Add swagger info to base controller class

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,23 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\OpenApi(
+ *     @OA\Info(
+ *         version="1.0.0",
+ *         title="Blog API",
+ *         description="API documentation for the Blog Application",
+ *         @OA\Contact(
+ *             email="admin@example.com"
+ *         )
+ *     ),
+ *     @OA\Server(
+ *         url="http://localhost:8000/api",
+ *         description="Local API Server"
+ *     )
+ * )
+ */
+
 abstract class Controller
 {
     //
