8fdbfb03bdbe11125545dc60df338b2251e11e1e
MoNashar <mohamad.nashar.maids.cc@gmail.com>
2025-05-03 15:32:03 +0300

    Group Routes

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,22 @@
 <?php
 
+use App\Http\Controllers\ActivityLogs;
 use App\Http\Controllers\Categories;
 use App\Http\Controllers\Posts;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/posts', [Posts::class, 'index'])->name('posts.index');
-Route::get('/posts/{post}', [Posts::class, 'show'])->name('posts.show');
+Route::group(['prefix' => 'posts'], function () {
+    Route::get('/', [Posts::class, 'index'])->name('posts.index');
+    Route::get('/{post}', [Posts::class, 'show'])->name('posts.show');
+    Route::post('/', [Posts::class, 'store'])->name('posts.store');
+    Route::put('/{post}', [Posts::class, 'update'])->name('posts.update');
+    Route::delete('/{post}', [Posts::class, 'destroy'])->name('posts.destroy');
+});
 
-Route::get('/categories', [Categories::class, 'index'])->name('categories.index');
-Route::get('/categories/{category:slug}', [Categories::class, 'show'])->name('categories.show');
-Route::post('/categories', [Categories::class, 'store'])->name('categories.store');
-Route::put('/categories/{category:slug}', [Categories::class, 'update'])->name('categories.update');
-Route::delete('/categories/{category:slug}', [Categories::class, 'destroy'])->name('categories.destroy');
+Route::group(['prefix' => 'categories'], function () {
+    Route::get('/', [Categories::class, 'index'])->name('categories.index');
+    Route::get('/{category:slug}', [Categories::class, 'show'])->name('categories.show');
+    Route::post('/', [Categories::class, 'store'])->name('categories.store');
+    Route::put('/{category:slug}', [Categories::class, 'update'])->name('categories.update');
+    Route::delete('/{category:slug}', [Categories::class, 'destroy'])->name('categories.destroy');
+});
